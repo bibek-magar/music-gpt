c98ef11592ead2039e489b623a9f0c2d207c0427
Bibek Magar <beevekmgr@gmail.com>
2025-12-30 23:13:12 +0545

    feat: Added sidebar promotional banner

--- components/molecules/PromotionalBanner.tsx
+++ components/molecules/PromotionalBanner.tsx
@@ -1,15 +1,17 @@
 export default function PromotionalBanner() {
   return (
     <div
-      className="rounded-xl flex flex-col opacity-100 px-3 py-2.5 h-[94px]"
+      className="rounded-[16px] flex flex-col px-4 py-3"
       style={{
-        background: `linear-gradient(233.67deg, rgba(48, 7, 255, 0.29) -2.43%, rgba(209, 40, 150, 0.271346) 58.32%, rgba(255, 86, 35, 0.25) 98.83%), linear-gradient(0deg, #1D2125, #1D2125)`,
+        // Paint the colorful gradient on top of the base layer
+        background:
+          "linear-gradient(233.67deg, rgba(48, 7, 255, 0.29) -2.43%, rgba(209, 40, 150, 0.271346) 58.32%, rgba(255, 86, 35, 0.25) 98.83%), linear-gradient(0deg, var(--Shades-Primary-200, #1D2125), var(--Shades-Primary-200, #1D2125))",
       }}
     >
-      <h3 className="text-white font-semibold text-[12px] leading-[146%] tracking-[0.01em]">
+      <h3 className="text-white font-semibold text-[12px] mb-1">
         Model v6 Pro is here!
       </h3>
-      <p className="text-[#FFFFFFA3] font-normal text-[12px] leading-[146%] tracking-[0.01em] mt-0.5">
+      <p className="text-text-secondary font-normal text-[12px]">
         Pushing boundaries to the world&apos;s best AI music model
       </p>
     </div>
